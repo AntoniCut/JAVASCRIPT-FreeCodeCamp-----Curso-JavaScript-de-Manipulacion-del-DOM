6be6334352805160327f07abc375eb44e85e0b93
Antonio <antonicut@gmail.com>
2024-07-20 14:33:55 +0200

    Terminar aplicación 06-todo-app

---
+++ 02-toppings-de-pizza/app.js
@@ -0,0 +1,21 @@
+//  *******************************
+//  **********  /app.js  **********
+//  *******************************
+
+
+//  **********  getElementById  **********
+
+const contenedor = document.getElementById('contenedor');
+console.log("contenedor: ", contenedor);
+console.log("contenedor innerHTML: ", contenedor.innerHTML);
+console.log("contenedor innerHTML - tipo de dato: ", typeof contenedor.innerHTML);
+
+
+const titulo = document.getElementById('titulo');
+console.log("\ntitulo: ", titulo);
+console.log("titulo - tipo de dato: ", typeof titulo);
+console.log("Texto del Título:", titulo.innerText);                 //  Texto del titulo
+console.log("Tipo de etiqueta del Título:", titulo.tagName);        //  Tipo de etiqueta
+
+
+//  **********  
